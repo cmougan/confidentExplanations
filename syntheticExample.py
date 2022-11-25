bdca899afb30d66087a3c8cf3563ab1a50556d21
Carlos Mougan <carmougan@gmail.com>
2022-11-25 11:03:49 +0100

    syntheticExample 2d visualization

--- syntheticExample.py
+++ syntheticExample.py
@@ -20,25 +20,16 @@ res_plugin_actual_cov = []
 values = np.linspace(0.1, 0.9, 9)
 for disp in values:
     # Create synthetic iid data that depends on a dispersion parameter
-    x1, x2 = np.random.multivariate_normal([0, 0], [[disp, 0], [0, disp]], 1000).T
-    x3, x4 = np.random.multivariate_normal([1, 1], [[disp, 0], [0, disp]], 1000).T
-    if plot:
-        plt.figure(figsize=(10, 10))
-        plt.scatter(x1, x2, alpha=0.2)
-        plt.scatter(x3, x4, alpha=0.2)
-        plt.show()
-    # Convert to dataframe, add labels, and split into train and test
-    X1 = pd.DataFrame([x1, x2]).T
-    X1["target"] = 0
-    X2 = pd.DataFrame([x3, x4]).T
-    X2["target"] = 1
-    X = pd.concat([X1, X2])
-    X.columns = ["var1", "var2", "target"]
+    X, y = make_blobs(
+        n_samples=2000, centers=2, n_features=2, random_state=0, cluster_std=disp
+    )
+    df = pd.DataFrame(X, columns=["var1", "var2"])
+    df["label"] = y
 
     X_tr, X_te, y_tr, y_te = train_test_split(
-        X.drop(columns="target"),
-        X["target"],
-        stratify=X["target"],
+        X,
+        y,
+        stratify=y,
         test_size=0.5,
         random_state=0,
     )
@@ -64,14 +55,34 @@ for disp in values:
     if accuracy_score(y_te, preds) > accuracy_score(y_te[selected], preds[selected]):
         print("SAX Accuracy over accepted instances is worse than overall accuracy")
     # Plug in
-    selected = plugIn.predict(X_te).astype(bool)
-    preds = plugIn.model.predict(X_te).astype(bool)
+    selected_plug = plugIn.predict(X_te).astype(bool)
+    preds_plug = plugIn.model.predict(X_te).astype(bool)
     res_plugIn.append(
-        accuracy_score(y_te[selected], preds[selected]),
+        accuracy_score(y_te[selected_plug], preds[selected_plug]),
     )
-    res_plugin_actual_cov.append(sum(selected) / len(selected))
-    if accuracy_score(y_te, preds) > accuracy_score(y_te[selected], preds[selected]):
+    res_plugin_actual_cov.append(sum(selected_plug) / len(selected_plug))
+    if accuracy_score(y_te, preds) > accuracy_score(
+        y_te[selected_plug], preds[selected_plug]
+    ):
         print("PlugIn Accuracy over accepted instances is worse than overall accuracy")
+
+    # Plots
+    plt.figure()
+    X1 = df[df["label"] == 1]
+    X0 = df[df["label"] == 0]
+    plt.scatter(X1["var1"], X1["var2"], alpha=0.1, label="Class0")
+    plt.scatter(X0["var1"], X0["var2"], alpha=0.1, label="Class1")
+    plt.scatter(
+        X_te[~selected_plug][:, 0],
+        X_te[~selected_plug][:, 1],
+        alpha=0.5,
+        label="PlugIn",
+    )
+    plt.scatter(
+        X_te[~selected][:, 0], X_te[~selected][:, 1], alpha=0.5, marker="+", label="SAX"
+    )
+    plt.legend()
+    plt.show()
 
 
 # %%
@@ -92,4 +103,3 @@ plt.legend()
 plt.xlabel("Dispersion")
 plt.ylabel("Accuracy")
 plt.show()
-# %%
