33d0c3bb42721246669382785933aefdf6a06f46
Carlos Mougan <carmougan@gmail.com>
2022-11-25 10:26:29 +0100

    remove this file, not used

--- selectionFunction.py
+++
@@ -1,9 +0,0 @@
-# %%
-import pandas as pd
-import numpy as np
-import lightgbm as lgb
-from sklearn.model_selection import train_test_split
-from attributes import *
-
-# here we import X features of dataset
-atts = atts_giveme
