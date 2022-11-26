e21e2fdf281244746f592b3b16b98da9ff0f3e86
Carlos Mougan <carmougan@gmail.com>
2022-11-26 18:53:16 +0100

    clean experiment

--- syntheticExample.py
+++ syntheticExample.py
@@ -24,11 +24,11 @@ res_sax_base = []
 res_sax_base_actual_cov = []
 res_plugin_actual_cov = []
 res_plugIn = []
-values = np.linspace(0.1, 1.5, 20)
+values = np.linspace(0.1, 2, 10)
 for disp in values:
     # Create synthetic iid data that depends on a dispersion parameter
-    X, y = make_blobs(n_samples=2000, centers=2, n_features=10, cluster_std=disp)
-    df = pd.DataFrame(X, columns=["Var%d" % (i + 1) for i in range(X_ood.shape[1])])
+    X, y = make_blobs(n_samples=2000, centers=2, n_features=2, cluster_std=disp)
+    df = pd.DataFrame(X, columns=["Var%d" % (i + 1) for i in range(X.shape[1])])
     df["label"] = y
 
     X_tr, X_te, y_tr, y_te = train_test_split(
@@ -37,21 +37,22 @@ for disp in values:
         stratify=y,
         test_size=0.5,
     )
-    F = LogisticRegression()
-    G = XGBClassifier()
+
     # Fit our detector
-    detector = SelectiveAbstentionExplanations(model=F, gmodel=G, cov=cov)
+    detector = SelectiveAbstentionExplanations(
+        model=LogisticRegression(), gmodel=XGBClassifier(), cov=cov
+    )
     detector.fit(X_tr, y_tr)
     # Detector baseline on prediction space instead of explanation space
     det_pred = SelectiveAbstentionExplanations(
-        model=F,
-        gmodel=G,
+        model=LogisticRegression(),
+        gmodel=XGBClassifier(),
         cov=cov,
         use_explanation_space=False,
     )
     det_pred.fit(X_tr, y_tr)
     # Fit plug in
-    plugIn = PlugInRule(model=F)
+    plugIn = PlugInRule(model=LogisticRegression())
     plugIn.fit(X_tr, y_tr)
 
     # Evaluation
@@ -80,29 +81,34 @@ for disp in values:
     res_plugin_actual_cov.append(sum(selected_plug) / len(selected_plug))
 
     # Plots
-    plt.figure(figsize=(10, 10))
-    X1 = df[df["label"] == 1]
-    X0 = df[df["label"] == 0]
-    plt.scatter(X1["var1"], X1["var2"], alpha=0.1, label="Class0")
-    plt.scatter(X0["var1"], X0["var2"], alpha=0.1, label="Class1")
-    plt.scatter(
-        X_te[~selected_plug][:, 0],
-        X_te[~selected_plug][:, 1],
-        alpha=0.5,
-        label="PlugIn",
-    )
-    plt.scatter(
-        X_te[~selected][:, 0], X_te[~selected][:, 1], alpha=0.5, marker="+", label="SAX"
-    )
-    plt.scatter(
-        X_te[~selected_base][:, 0],
-        X_te[~selected_base][:, 1],
-        alpha=0.5,
-        marker=".",
-        label="SAX Base",
-    )
-    plt.legend()
-    plt.show()
+    if plot:
+        plt.figure(figsize=(10, 10))
+        X1 = df[df["label"] == 1]
+        X0 = df[df["label"] == 0]
+        plt.scatter(X1["Var1"], X1["Var2"], alpha=0.1, label="Class0")
+        plt.scatter(X0["Var1"], X0["Var2"], alpha=0.1, label="Class1")
+        plt.scatter(
+            X_te[~selected_plug][:, 0],
+            X_te[~selected_plug][:, 1],
+            alpha=0.5,
+            label="PlugIn",
+        )
+        plt.scatter(
+            X_te[~selected][:, 0],
+            X_te[~selected][:, 1],
+            alpha=0.5,
+            marker="+",
+            label="SAX",
+        )
+        plt.scatter(
+            X_te[~selected_base][:, 0],
+            X_te[~selected_base][:, 1],
+            alpha=0.5,
+            marker=".",
+            label="SAX Base",
+        )
+        plt.legend()
+        plt.show()
 # %%
 plt.figure(figsize=(10, 10))
 plt.title("Accuracy over accepted instances with coverage = {}".format(cov))
